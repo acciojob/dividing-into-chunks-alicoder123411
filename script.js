602d2e213012d1e00b88b93a3420ca7817308e6a
alicoder123411 <sameersingla240@gmail.com>
2026-01-20 15:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	let ch = [];
+	let c = arr.length;
+
+	for (let i = 0; i < c; i += n) {
+		ch.push(arr.slice(i, i + n));
+	}
+
+	return ch;
 };
 
-const n = prompt("Enter n: ");
+const n = Number(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
